96ca1ab6c7a6470b32e152111a122216c70182d8
GaaiChyunLu <lujiaquan66@icloud.com>
2021-10-10 21:05:10 +0800

    Commit 01

---
+++ Project_01/Project_01/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project_01
+//
+//  Created by 卢佳泉 on 2021/10/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project_01/Project_01/Project_01App.swift
@@ -0,0 +1,17 @@
+//
+//  Project_01App.swift
+//  Project_01
+//
+//  Created by 卢佳泉 on 2021/10/10.
+//
+
+import SwiftUI
+
+@main
+struct Project_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
